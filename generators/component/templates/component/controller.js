864ccbdd2671ae52f1385089712282ff79000b40
Thomas Hynard <tomhynard@gmail.com>
2016-01-07 08:05:03 +1100

    Updating generator to use new controller format

--- generators/component/templates/component/controller.js
+++ generators/component/templates/component/controller.js
@@ -1,11 +1,9 @@
-const $inject = ['settingsService'];
-const <%= title %>Ctrl = function (settingsService) {
+const $inject = ['componentService'];
+
+const <%= title %>Ctrl = function (componentService) {
   let vm = this;
 
-  let data = settingsService.findObject(vm.lookUp, vm.data, vm.settings, vm.index);
-
-  vm.modelData = data[0];
-  vm.submitTo = data[1];
+  angular.extend(vm, componentService.getProps(vm));
 };
 
 <%= title %>Ctrl.$inject = $inject;
